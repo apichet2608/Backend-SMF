abcf1d4b8099434d7cc1c58999c98308eba8b525
apichet2608 <Apichet.jtd@outlook.com>
2023-06-22 17:27:17 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/foxsystem_json_backup_header_summary.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/foxsystem_json_backup_header_summary.js
@@ -75,8 +75,7 @@ GROUP BY
 
 router.get("/data-fix", async (req, res) => {
   try {
-    const { product1, product2, product3, product4, startdate, stopdate } =
-      req.query;
+    const { product, startdate, stopdate } = req.query;
 
     const result = await query(
       `SELECT
@@ -96,7 +95,7 @@ router.get("/data-fix", async (req, res) => {
       GROUP BY
         production_date,
         sendresultdetails_product`,
-      [product1, product2, product3, product4, startdate, stopdate]
+      [product, startdate, stopdate]
     );
 
     res.status(200).json(result.rows);
