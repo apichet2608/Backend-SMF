dcfbf3a503b859d6289021e61d7c0f81d2d9ae05
apichet2608 <Apichet.jtd@outlook.com>
2023-06-23 08:45:51 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/foxsystem_json_backup_header_ok.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/foxsystem_json_backup_header_ok.js
@@ -65,6 +65,44 @@ router.get("/product-fail", async (req, res) => {
   }
 });
 
+router.get("/sum-fail-groupby-process", async (req, res) => {
+  try {
+    const { startdate, stopdate, product } = req.query;
+
+    const result = await query(
+      `select
+      station_process,
+      COUNT(*) as count
+    from
+      (
+      select
+        *
+      from
+        public.foxsystem_json_backup_header_ok
+      where
+        test_attributes_uut_stop >= $1
+        and DATE_TRUNC('day',
+        test_attributes_uut_stop) <= DATE_TRUNC('day',
+        $2::TIMESTAMP)
+        and sendresultdetails_product = $3
+        and test_attributes_test_result = 'FAIL'
+      order by
+        test_attributes_uut_stop
+    ) subquery
+    group by
+      station_process
+    order by count asc
+    `,
+      [startdate, stopdate, product]
+    );
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
 router.get("/distinct-sendresultdetails_product", async (req, res) => {
   try {
     const result = await query(`
