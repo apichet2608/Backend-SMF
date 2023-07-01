2cf1d715147ff75dbd1cf61853a1a2a15d4f021f
apichet2608 <Apichet.jtd@outlook.com>
2023-07-01 17:26:57 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
@@ -16,67 +16,96 @@ router.get("/count-status", async (req, res) => {
   try {
     const result = await query(`
     WITH aggregated_data AS (
-      SELECT
+        SELECT
           building,
           json_agg(json_build_object(
-              'month_code',
-              month_code,
-              'building',
-              building,
-              'total_diff_energy_usage',
-              total_diff_energy_usage
+            'month_code',
+            month_code,
+            'building',
+            building,
+            'total_diff_energy_usage',
+            total_diff_energy_usage,
+            'energy_cost_baht',
+            total_energy_cost_baht
           )) AS data
-      FROM
+        FROM
           (
-              SELECT
-                  month_code,
-                  building,
-                  SUM(diff_energy_usage) AS total_diff_energy_usage
-              FROM
-                  public.smart_energy_by_month
-              GROUP BY
-                  month_code,
-                  building
-              ORDER BY
-                  building ASC,
-                  month_code ASC
-          ) AS subquery
-      GROUP BY
-          building
-      HAVING
-          COUNT(*) > 1
-  )
-  SELECT
-      t1.month_code,
-      t1.total_diff_energy_usage,
-      t2.building,
-      t2.data
-  FROM
-      (
-          SELECT
+            SELECT
               month_code,
               building,
-              SUM(diff_energy_usage) AS total_diff_energy_usage
-          FROM
-              public.smart_energy_by_month AS t1
-          WHERE
-              NOT EXISTS (
-                  SELECT
-                      1
-                  FROM
-                      public.smart_energy_by_month AS t2
-                  WHERE
-                      t2.building = t1.building
-                      AND t2.month_code > t1.month_code
-              )
-          GROUP BY
+              SUM(diff_energy_usage) AS total_diff_energy_usage,
+              SUM(energy_cost_baht) AS total_energy_cost_baht
+            FROM
+              public.smart_energy_by_month
+            GROUP BY
               month_code,
               building
-          ORDER BY
+            ORDER BY
               building ASC,
               month_code ASC
-      ) AS t1
-  INNER JOIN aggregated_data AS t2 ON t2.building = t1.building;  
+          ) AS subquery
+        GROUP BY
+          building
+        HAVING
+          COUNT(*) > 1
+      )
+      SELECT
+        t1.month_code,
+        t1.total_diff_energy_usage,
+        t1.total_energy_cost_baht,
+        t2.building,
+        t2.data
+      FROM
+        (
+          WITH aggregated_data AS (
+            SELECT
+              month_code,
+              building,
+              SUM(diff_energy_usage) AS total_diff_energy_usage,
+              SUM(energy_cost_baht) AS total_energy_cost_baht
+            FROM
+              public.smart_energy_by_month AS t1
+            WHERE
+              NOT EXISTS (
+                SELECT
+                  1
+                FROM
+                  public.smart_energy_by_month AS t2
+                WHERE
+                  t2.building = t1.building
+                  AND t2.month_code > t1.month_code
+              )
+            GROUP BY
+              month_code,
+              building
+          )
+          SELECT
+            month_code,
+            building,
+            SUM(total_diff_energy_usage) AS total_diff_energy_usage,
+            SUM(total_energy_cost_baht) AS total_energy_cost_baht
+          FROM
+            aggregated_data
+          GROUP BY
+            month_code,
+            building
+      
+          UNION ALL
+      
+          SELECT
+            month_code,
+            'total' AS building,
+            SUM(total_diff_energy_usage) AS total_diff_energy_usage,
+            SUM(total_energy_cost_baht) AS total_energy_cost_baht
+          FROM
+            aggregated_data
+          GROUP BY
+            month_code
+          ORDER BY
+            month_code,
+            building
+        ) AS t1
+        INNER JOIN aggregated_data AS t2 ON t2.building = t1.building;       
     `);
     res.status(200).json(result.rows);
   } catch (error) {
