5dd676adcf0a3bbfa0391da760aecbb55ff6dd76
apichet2608 <Apichet.jtd@outlook.com>
2023-07-03 10:57:42 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
@@ -15,54 +15,51 @@ const query = (text, params) => pool.query(text, params);
 router.get("/count-status", async (req, res) => {
   try {
     const result = await query(`
-    WITH aggregated_data AS (
-      SELECT
-          ROW_NUMBER() OVER (ORDER BY month_code ASC) AS id,
-          month_code,
-          building,
-          SUM(diff_energy_usage) AS total_diff_energy_usage,
-          SUM(energy_cost_baht) AS total_energy_cost_baht
-      FROM
-          public.smart_energy_by_month
-      WHERE
-          NOT EXISTS (
-              SELECT
-                  1
-              FROM
-                  public.smart_energy_by_month AS t2
-              WHERE
-                  t2.building = public.smart_energy_by_month.building
-                  AND t2.month_code > public.smart_energy_by_month.month_code
+    with aggregated_data as (
+        select
+            month_code,
+            building,
+            SUM(diff_energy_usage) as total_diff_energy_usage,
+            SUM(energy_cost_baht) as total_energy_cost_baht
+        from
+            public.smart_energy_by_month
+        where
+            not exists (
+            select
+                1
+            from
+                public.smart_energy_by_month as t2
+            where
+                t2.building = public.smart_energy_by_month.building
+                and t2.month_code > public.smart_energy_by_month.month_code
+                  )
+        group by
+            month_code,
+            building
           )
-      GROUP BY
-          month_code,
-          building
-  )
-  SELECT
-      id,
-      month_code,
-      building,
-      SUM(total_diff_energy_usage) AS total_diff_energy_usage,
-      SUM(total_energy_cost_baht) AS total_energy_cost_baht
-  FROM
-      aggregated_data
-  GROUP BY
-      id, month_code, building
-  
-  UNION ALL
-  
-  SELECT
-      ROW_NUMBER() OVER (ORDER BY month_code ASC) AS id,
-      month_code,
-      'total' AS building,
-      SUM(total_diff_energy_usage) AS total_diff_energy_usage,
-      SUM(total_energy_cost_baht) AS total_energy_cost_baht
-  FROM
-      aggregated_data
-  GROUP BY
-      month_code
-  ORDER BY
-      month_code, building;  
+        select
+            month_code,
+            building,
+            SUM(total_diff_energy_usage) as total_diff_energy_usage,
+            SUM(total_energy_cost_baht) as total_energy_cost_baht
+        from
+            aggregated_data
+        group by
+            month_code,
+            building
+        union all
+        select
+            month_code,
+            'total' as building,
+            SUM(total_diff_energy_usage) as total_diff_energy_usage,
+            SUM(total_energy_cost_baht) as total_energy_cost_baht
+        from
+            aggregated_data
+        group by
+            month_code
+        order by
+            month_code,
+            building;    
     `);
     res.status(200).json(result.rows);
   } catch (error) {
@@ -177,6 +174,44 @@ limit 10
     }
 
     const result = await query(queryStr, queryParams);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+router.get("/page2/table", async (req, res) => {
+  try {
+    const result = await query(`
+    select
+	mdb_code,
+	feeder,
+	maker_mdb_code,
+	mc_equip_name,
+	energy_use_type,
+	load_type,
+	"desc",
+	building,
+	cost_ceter,
+	room_type,
+	dept_1,
+	dept_2,
+	area,
+	power_ratio,
+	max_energy_use,
+	energy_usage,
+	"month",
+	month_code,
+	max_date,
+	unit_price_bth_per_kwh,
+	diff_energy_usage,
+	energy_cost_baht,
+	id,
+	auto_check
+from
+	public.smart_energy_by_month; 
+    `);
     res.status(200).json(result.rows);
   } catch (error) {
     console.error(error);
