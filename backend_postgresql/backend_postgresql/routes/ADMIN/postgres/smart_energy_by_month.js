fd6e5914e0916e255b38aab4720743df7ac310b1
apichet2608 <Apichet.jtd@outlook.com>
2023-07-03 12:03:37 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
@@ -183,35 +183,66 @@ limit 10
 
 router.get("/page2/table", async (req, res) => {
   try {
-    const result = await query(`
-    select
-	mdb_code,
-	feeder,
-	maker_mdb_code,
-	mc_equip_name,
-	energy_use_type,
-	load_type,
-	"desc",
-	building,
-	cost_ceter,
-	room_type,
-	dept_1,
-	dept_2,
-	area,
-	power_ratio,
-	max_energy_use,
-	energy_usage,
-	"month",
-	month_code,
-	max_date,
-	unit_price_bth_per_kwh,
-	diff_energy_usage,
-	energy_cost_baht,
-	id,
-	auto_check
+    const { build } = req.query;
+
+    let queryStr = "";
+    let queryParams = [];
+
+    if (build === "ALL") {
+      queryStr = `
+      select
+	row_number() over (
+	order by month_code asc) as id,
+		load_type,
+		building,
+		month_code,
+		sum(energy_usage) as energy_usage_count
 from
-	public.smart_energy_by_month; 
-    `);
+	public.smart_energy_by_month
+where
+	month_code = (
+	select
+		MAX(month_code)
+	from
+		public.smart_energy_by_month)
+group by 
+		load_type,
+		building,
+		month_code
+order by
+		 energy_usage_count desc,
+		 load_type desc
+        `;
+    } else {
+      queryStr = `
+      select
+	row_number() over (
+	order by month_code asc) as id,
+		load_type,
+		building,
+		month_code,
+		sum(energy_usage) as energy_usage_count
+from
+	public.smart_energy_by_month
+where
+	month_code = (
+	select
+		MAX(month_code)
+	from
+		public.smart_energy_by_month)
+    and building = $1
+group by 
+		load_type,
+		building,
+		month_code
+order by
+		 energy_usage_count desc,
+		 load_type desc
+        `;
+      queryParams = [build];
+    }
+
+    const result = await query(queryStr, queryParams);
     res.status(200).json(result.rows);
   } catch (error) {
     console.error(error);
