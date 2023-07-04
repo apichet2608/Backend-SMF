e877676f38618b24f6f9ad1283a165da4f561784
apichet2608 <Apichet.jtd@outlook.com>
2023-07-04 13:58:41 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_month.js
@@ -489,4 +489,97 @@ router.get("/page3/table2", async (req, res) => {
     res.status(500).json({ error: "An error occurred while fetching data" });
   }
 });
+
+router.get("/page3/distinctbuild", async (req, res) => {
+  try {
+    const { dept, loadtype } = req.query;
+
+    let queryStr = "";
+    let queryParams = [];
+
+    if (dept === "ALL") {
+      queryStr = `
+      select
+	distinct building 
+from
+	public.smart_energy_by_month
+where 
+      load_type = $1
+        `;
+      queryParams = [loadtype];
+    } else {
+      queryStr = `
+      select
+      distinct building 
+    from
+      public.smart_energy_by_month
+    where 
+      dept_2 = $1
+      and
+      load_type = $2
+        `;
+      queryParams = [dept, loadtype];
+    }
+
+    const result = await query(queryStr, queryParams);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+router.get("/page3/plot2", async (req, res) => {
+  try {
+    const { build, loadtype, dept } = req.query;
+
+    let queryStr = "";
+    let queryParams = [];
+
+    if (dept === "ALL") {
+      queryStr = `
+      select
+      month_code,
+      sum(diff_energy_usage) as diff_energy_usage
+    from
+      public.smart_energy_by_month
+    where 
+      building = $1
+      and
+      load_type = $2
+    group by 
+      month_code 
+    order by
+         month_code asc
+        `;
+      queryParams = [build, loadtype];
+    } else {
+      queryStr = `
+      select
+      month_code,
+      sum(diff_energy_usage) as diff_energy_usage
+    from
+      public.smart_energy_by_month
+    where 
+      building = $1
+      and
+      load_type = $2
+      and 
+      dept_2 = $3
+    group by 
+      month_code 
+    order by
+         month_code asc
+        `;
+      queryParams = [build, loadtype, dept];
+    }
+
+    const result = await query(queryStr, queryParams);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
 module.exports = router;
