e99a55c678fcef22f1df72d110e09e78fa0d0a90
apichet2608 <Apichet.jtd@outlook.com>
2023-07-04 09:03:16 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_machine_connect_list.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_machine_connect_list.js
@@ -203,13 +203,13 @@ router.put("/:id", async (req, res) => {
       status,
       npi_year,
       plan_date,
+      finish_date,
       remark,
       feeder_no,
     } = req.body;
 
-    if (status === "Finished") {
-      const result = await query(
-        `UPDATE public.smart_machine_connect_list
+    const result = await query(
+      `UPDATE public.smart_machine_connect_list
        SET
          item_factory = $1,
          item_code = $2,
@@ -231,90 +231,38 @@ router.put("/:id", async (req, res) => {
          status = $18,
          npi_year = $19,
          plan_date = $20,
-         remark = $21,
-         feeder_no = $22,
-         finished_date = now()
-       WHERE id = $23;`,
-        [
-          item_factory,
-          item_code,
-          item_desc1,
-          item_desc2,
-          item_desc3,
-          item_status,
-          item_building,
-          item_group,
-          item_sub_group,
-          item_owner_cc,
-          item_sub_process,
-          item_mac_maker,
-          item_iot_mc_type,
-          item_iot_group1,
-          item_iot_cont1,
-          item_iot_group2,
-          item_iot_cont2,
-          status,
-          npi_year,
-          plan_date,
-          remark,
-          feeder_no,
-          id,
-        ]
-      );
-    } else {
-      const result = await query(
-        `UPDATE public.smart_machine_connect_list
-       SET
-         item_factory = $1,
-         item_code = $2,
-         item_desc1 = $3,
-         item_desc2 = $4,
-         item_desc3 = $5,
-         item_status = $6,
-         item_building = $7,
-         item_group = $8,
-         item_sub_group = $9,
-         item_owner_cc = $10,
-         item_sub_process = $11,
-         item_mac_maker = $12,
-         item_iot_mc_type = $13,
-         item_iot_group1 = $14,
-         item_iot_cont1 = $15,
-         item_iot_group2 = $16,
-         item_iot_cont2 = $17,
-         status = $18,
-         npi_year = $19,
-         plan_date = $20,
-         remark = $21,
-         feeder_no = $22
-       WHERE id = $23;`,
-        [
-          item_factory,
-          item_code,
-          item_desc1,
-          item_desc2,
-          item_desc3,
-          item_status,
-          item_building,
-          item_group,
-          item_sub_group,
-          item_owner_cc,
-          item_sub_process,
-          item_mac_maker,
-          item_iot_mc_type,
-          item_iot_group1,
-          item_iot_cont1,
-          item_iot_group2,
-          item_iot_cont2,
-          status,
-          npi_year,
-          plan_date,
-          remark,
-          feeder_no,
-          id,
-        ]
-      );
-    }
+         finish_date = $21,
+         remark = $22,
+         feeder_no = $23
+       WHERE id = $24;`,
+      [
+        item_factory,
+        item_code,
+        item_desc1,
+        item_desc2,
+        item_desc3,
+        item_status,
+        item_building,
+        item_group,
+        item_sub_group,
+        item_owner_cc,
+        item_sub_process,
+        item_mac_maker,
+        item_iot_mc_type,
+        item_iot_group1,
+        item_iot_cont1,
+        item_iot_group2,
+        item_iot_cont2,
+        status,
+        npi_year,
+        plan_date,
+        finish_date,
+        remark,
+        feeder_no,
+        id,
+      ]
+    );
+
     res.status(200).json({ message: "Data updated successfully" });
   } catch (error) {
     console.error(error);
@@ -362,6 +310,7 @@ router.post("/", async (req, res) => {
       status,
       npi_year,
       plan_date,
+      finish_date,
       remark,
       feeder_no,
       item_desc1,
@@ -390,6 +339,7 @@ router.post("/", async (req, res) => {
       status,
       npi_year,
       plan_date,
+      finish_date,
       remark,
       feeder_no,
       item_desc1)
@@ -414,7 +364,8 @@ router.post("/", async (req, res) => {
     $19,
     $20,
     $21,
-    $22);`,
+    $22,
+    $23);`,
       [
         item_factory,
         item_code,
@@ -435,6 +386,7 @@ router.post("/", async (req, res) => {
         status,
         npi_year,
         plan_date,
+        finish_date,
         remark,
         feeder_no,
         item_desc1,
