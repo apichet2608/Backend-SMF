986879c2a948e1c71551666e0cd3b7b2e71fa733
apichet2608 <Apichet.jtd@outlook.com>
2023-07-26 14:23:47 +0700

    dev

---
+++ backend_postgresql/backend_postgresql/routes/10.17.66.121/iot/iot.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "10.17.66.121",
+  port: 5432,
+  user: "-",
+  password: "-",
+  database: "iot", // แทนที่ด้วยชื่อฐานข้อมูลของคุณ
+});
+
+const query = (text, params) => pool.query(text, params);
+
+//
+module.exports = router;
