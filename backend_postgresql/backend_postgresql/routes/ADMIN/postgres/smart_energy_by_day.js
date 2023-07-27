79ee334f6f80ceb59c49d6bfe8cc28d525c9d7cd
apichet2608 <Apichet.jtd@outlook.com>
2023-07-27 16:42:05 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_day.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_energy_by_day.js
@@ -29,4 +29,53 @@ router.get("/page6/distinctbuilding", async (req, res) => {
   }
 });
 
+router.get("/page6/distinctdept2", async (req, res) => {
+  try {
+    const { building } = req.query;
+
+    const queryStr = `
+    select
+	distinct dept_2
+from
+	public.smart_energy_by_day
+where
+	building = $1
+order by
+	dept_2 asc
+    `;
+    const queryParams = [building];
+
+    const result = await query(queryStr, queryParams);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+router.get("/page6/distinctload_type", async (req, res) => {
+  try {
+    const { building, dept_2 } = req.query;
+
+    const queryStr = `
+    select
+	distinct load_type
+from
+	public.smart_energy_by_day
+where
+	building = $1
+and dept_2  = $2
+order by
+	load_type asc
+    `;
+    const queryParams = [building, dept_2];
+
+    const result = await query(queryStr, queryParams);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
 module.exports = router;
