f75f6958d23d23b7735a7b77d7f091d2ccf65356
apichet2608 <Apichet.jtd@outlook.com>
2023-08-02 14:32:04 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/10.17.77.118/iot/smart_eworking_calling.js
+++ backend_postgresql/backend_postgresql/routes/10.17.77.118/iot/smart_eworking_calling.js
@@ -27,4 +27,78 @@ router.get("/distinctjwpv_dept", async (req, res) => {
   }
 });
 
+router.get("/distinctjwpv_proc_group", async (req, res) => {
+  try {
+    const { jwpv_dept } = req.query;
+    let queryStr = "";
+    let queryParams = [];
+
+    queryStr = `
+      select
+      distinct jwpv_proc_group
+    from
+      public.smart_eworking_calling
+    where jwpv_dept  = $1
+        `;
+    queryParams = [jwpv_dept];
+    const result = await query(queryStr, queryParams);
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+router.get("/distinctjwpv_job_type", async (req, res) => {
+  try {
+    const { jwpv_dept, jwpv_proc_group } = req.query;
+    let queryStr = "";
+    let queryParams = [];
+
+    queryStr = `
+    select
+    distinct jwpv_job_type
+  from
+    public.smart_eworking_calling
+  where
+    jwpv_dept = $1
+    and jwpv_proc_group = $2
+        `;
+    queryParams = [jwpv_dept, jwpv_proc_group];
+    const result = await query(queryStr, queryParams);
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+router.get("/distinctjwpv_mc_code", async (req, res) => {
+  try {
+    const { jwpv_dept, jwpv_proc_group, jwpv_job_type } = req.query;
+    let queryStr = "";
+    let queryParams = [];
+
+    queryStr = `
+    select
+	distinct jwpv_mc_code
+from
+	public.smart_eworking_calling
+where
+	jwpv_dept = $1
+	and jwpv_proc_group = $2
+	and jwpv_job_type = $3
+        `;
+    queryParams = [jwpv_dept, jwpv_proc_group, jwpv_job_type];
+    const result = await query(queryStr, queryParams);
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
 module.exports = router;
