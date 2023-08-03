b7c194074191179af3b75ab36c2e1bc8afb3ddb3
apichet2608 <Apichet.jtd@outlook.com>
2023-08-03 10:54:56 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/foxsystem_json_backup_header_ok.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/foxsystem_json_backup_header_ok.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "127.0.0.1",
+  host: "10.17.77.111",
   port: 5432,
   user: "postgres",
   password: "postgres",
