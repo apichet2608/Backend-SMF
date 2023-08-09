d0602348d0dd9b8e2bf3cc769e7cd5fc264f3845
Apichet Juntodum <131334081+apichet2608@users.noreply.github.com>
2023-08-09 16:50:16 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_master_verify_zaoi.js
+++ backend_postgresql/backend_postgresql/routes/ADMIN/postgres/smart_master_verify_zaoi.js
@@ -28,6 +28,32 @@ router.get("/page1/distinctsheet_no", async (req, res) => {
   }
 });
 
+
+router.get("/page1/distinctmachine_no", async (req, res) => {
+  try {
+    const { sheet_no } = req.query;
+
+    const queryStr = `
+      select
+        distinct machine_no
+      from
+        public.smart_master_verify_zaoi
+      where sheet_no = $1
+      order by machine_no desc
+    `;
+
+    const queryParams = [sheet_no];
+
+    const resultRows = await query(queryStr, queryParams);
+    res.status(200).json(resultRows.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+
+
 // router.get("/page1/distinctfixture_code", async (req, res) => {
 //   try {
 //     const { sheet_no } = req.query;
@@ -58,7 +84,7 @@ router.get("/page1/table", async (req, res) => {
     SELECT
     ROW_NUMBER() OVER () AS id,
 t.aoi_inspect_date,
-t.sheet_no,
+t.master_sheet_no,
 t.aoi_inspect_count,
 t.machine_no,
 CASE
@@ -66,7 +92,7 @@ WHEN NOT EXISTS (
 SELECT 1
 FROM smart_master_verify_zaoi sub
 WHERE sub.aoi_inspect_date = t.aoi_inspect_date
-AND sub.sheet_no = t.sheet_no
+AND sub.master_sheet_no = t.master_sheet_no
 AND sub.aoi_inspect_count = t.aoi_inspect_count
 AND sub.judgement <> 'PASS'
 ) THEN 'PASS'
@@ -75,12 +101,12 @@ END AS judgement
 FROM 
 smart_master_verify_zaoi t
 where 
-    sheet_no = $1
+    master_sheet_no = $1
     and aoi_inspect_date :: date >= $2 
     and aoi_inspect_date :: date <= $3 
 GROUP BY
 t.aoi_inspect_date,
-t.sheet_no,
+t.master_sheet_no,
 t.aoi_inspect_count,
 t.machine_no
 ORDER BY
