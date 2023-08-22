3ba8693db6a82ceea53d26b09bfb18ae070647c6
apichet2608 <Apichet.jtd@outlook.com>
2023-08-22 11:23:35 +0700

    dev

--- backend_postgresql/backend_postgresql/routes/10.17.71.21/10.17.71.21-data_log/smt_reflow_smic_actv.js
+++ backend_postgresql/backend_postgresql/routes/10.17.71.21/10.17.71.21-data_log/smt_reflow_smic_actv.js
@@ -33,19 +33,14 @@ router.get("/distinctmachine_code", async (req, res) => {
   try {
     const { line } = req.query;
     const result = await query(
-      `select
-      distinct machine_code 
-    from
-      public.smt_reflow_smic_actv
-    where line = $1
-    order by
-      machine_code asc`
+      `select distinct machine_code 
+       from public.smt_reflow_smic_actv
+       where line = $1
+       order by machine_code asc`,
+      [line]
     );
 
-    const queryParams = [line];
-
-    const resultRows = await query(queryStr, queryParams);
-    res.status(200).json(resultRows.rows);
+    res.status(200).json(result.rows);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while fetching data" });
@@ -54,59 +49,26 @@ router.get("/distinctmachine_code", async (req, res) => {
 
 router.get("/dataplot", async (req, res) => {
   try {
-    const { machine_code } = req.query;
+    const { line, machine_code } = req.query;
     const hours = parseInt(req.query.hours); // ชั่วโมงที่ผู้ใช้กำหนด
 
     if (isNaN(hours)) {
       return res.status(400).send("Hours are required");
     }
     const result = await query(
-      `
-      select
-        id,
-        machine_code,
-        log_level,
-        create_at,
-        "source",
-        "1t_pv_oc"  as t1_pv,
-        "1b_pv_oc"  as b1_pv ,
-        "2t_pv_oc"  as t2_pv,	
-        "2b_pv_oc"  as b2_pv,
-        "3t_pv_oc"  as t3_pv,
-        "3b_pv_oc"  as b3_pv,
-        "4t_pv_oc"  as t4_pv,
-        "4b_pv_oc"  as b4_pv,
-        "5t_pv_oc"  as t5_pv,
-        "5b_pv_oc"  as b5_pv,
-        "6t_pv_oc"  as t6_pv,
-        "6b_pv_oc"  as b6_pv,
-        "7t_pv_oc"  as t7_pv,
-        "7b_pv_oc"  as b7_pv,
-        "8t_pv_oc"  as t8_pv,
-        "8b_pv_oc"  as b8_pv,
-        zone7_o2_con_pv,
-        zone7_o2_con_pv_chat,
-        o2_con_pv,
-        o2_con_p_chat,
-        "zone",
-        zone_conveyor_speed_pv,
-        status_mc,
-        inside_mc,
-        total_input,
-        count_alarm
-      from
-        public.smt_reflow_tamura_temp_log
-      where
-        status_mc = 2
-        and inside_mc != 0
-        and zone7_o2_con_pv_chat > '0'
-        and machine_code = $1
-        and o2_con_p_chat < 1000
-      and create_at >= NOW() - interval '${hours}' hour
-      order by
-        create_at asc
+      `select
+      *
+    from
+      public.smt_reflow_smic_actv
+    where
+      line = $1
+      and machine_code = $2
+       and datetime >= NOW() - interval '${hours}' hour
+       and machine_con = 'Start'
+    order by
+      datetime asc
       `,
-      [machine_code]
+      [line, machine_code]
     );
     res.status(200).json(result.rows);
   } catch (error) {
