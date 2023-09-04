1d612c16cdf2e0e5b407d9359f4d90af06d98bae
apichet2608 <Apichet.jtd@outlook.com>
2023-09-04 11:47:21 +0700

    add api  smart_qa_aql_defect_master

--- backend_postgresql/backend_postgresql/app.js
+++ backend_postgresql/backend_postgresql/app.js
@@ -76,7 +76,7 @@ const smart_status_data_script_transform = require("./routes/ADMIN/postgres/smar
 const smart_qa_aql_record = require("./routes/10.17.77.118/iot/smart_qa_aql_record");
 const mdb_energy_master_result = require("./routes/10.17.66.121/iot/mdb_energy_master_result");
 const lpi_screen_tension = require("./routes/10.17.71.57/10.17.71.57-smart_factory/lpi_screen_tension");
-
+const smart_qa_aql_defect_master = require("./routes/10.17.77.118/iot/smart_qa_aql_defect_master");
 
 const app = express();
 
@@ -170,7 +170,7 @@ app.use(
 app.use("/api/smart_qa_aql_record", smart_qa_aql_record);
 app.use("/api/mdb_energy_master_result", mdb_energy_master_result);
 app.use("/api/lpi_screen_tension", lpi_screen_tension);
-
+app.use("/api/smart_qa_aql_defect_master", smart_qa_aql_defect_master);
 
 app.use(function (req, res, next) {
   next(createError(404));
---
+++ backend_postgresql/backend_postgresql/routes/10.17.77.118/iot/smart_qa_aql_defect_master.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "10.17.77.118",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "iot",
+});
+
+const query = (text, params) => pool.query(text, params);
+
+router.get("/distinctdefect_code", async (req, res) => {
+  try {
+    const result = await query(
+      `select
+      distinct defect_code
+    from
+      public.smart_qa_aql_defect_master
+    order by defect_code asc     
+    `
+    );
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+module.exports = router;
